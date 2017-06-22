84d784a29dd5a6f58938c0c02b8d31b6b1a2a46b
CycloneBoy <xuanfeng1992@gmail.com>
2017-06-22 09:18:44 +0800

    Initial commit

---
+++ src/main/java/com/cycloneboy/chapter20/TestCollection.java
@@ -0,0 +1,10 @@
+package com.cycloneboy.chapter20;
+
+/**
+ * Created by CycloneBoy on 2017/6/21.
+ */
+public class TestCollection {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
