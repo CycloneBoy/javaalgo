d23f2eec75e68c429712a548f4a98dc186de24c0
cycloneboy <xuanfeng1992@gmail.com>
2019-12-18 21:30:11 +0800

    添加数<<据结构与算法与问题求解JAVA语言描述>>代码

---
+++ java-algo/src/main/java/com/cycloneboy/data/table/Main.java
@@ -0,0 +1,9 @@
+package com.cycloneboy.data.table;
+
+/** Create by sl on 2019-11-30 22:53 */
+public class Main {
+
+  public static void main(String[] args) {
+    //    Collection<Integer>
+  }
+}
